d6839a89f79f007899840eb9dad887c0119fb864
RochelKnopf <ruchiknopf@gmail.com>
2022-07-28 14:07:03 -0500

    Homework 64

---
+++ js/64/hw.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var name = 'Ruchi';
+var email = 'ruchiknopf@gmail.com';
+
---
+++ js/64/hw2.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/* globals*/
+console.log(name, email);
+
+var year = prompt('Enter current year:');
+console.log('Current year: ' + year);
